d7b45f96b17da0f2b24975202a0dc5e3f5d09fe4
KaiCheng <1358798471@qq.com>
2018-09-29 17:54:14 +0800

    ck

--- TestDemo/Program.cs
+++ TestDemo/Program.cs
@@ -1,20 +1,45 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
 using System.Threading.Tasks;
 
 namespace TestDemo
 {
     class Program
     {
+        public char FindTheDifference(string s, string t)
+        {
+            if (t == "")
+                return new char();
+            var item = s.GroupBy(x => x).Select(q => new { key = q.Key, keyCnt = q.Count() });
+            var item2 = t.GroupBy(x => x).Select(q => new { key = q.Key, keyCnt = q.Count() });
+
+            var apk = (from a in item2
+                       join d in item on a.key equals d.key into temp
+                       from tt in temp.DefaultIfEmpty()
+                       where a.keyCnt > (tt == null ? 0 : tt.keyCnt)
+                       select new
+                       {
+                           a.key
+                       }).ToList();
+
+            if (apk == null)
+                return new char();
+            else
+                return apk[0].key;
+        }
+
         static void Main(string[] args)
         {
             //测试控制器
             //string str = "测试控制器2.0";
             //Console.WriteLine(str);
             //Console.ReadKey();
-
+            Mongo();
 
             Order or = new Order();
             or.id = 1;
@@ -46,6 +71,50 @@ namespace TestDemo
             Console.ReadKey();
         }
 
+        public static void Mongo()
+        {
+            //建立连接
+            var client = new MongoClient();
+            //建立数据库
+            var database = client.GetDatabase("TestDb");
+            //建立collection
+            var collection = database.GetCollection<BsonDocument>("foo");
+
+            var document = new BsonDocument
+            {
+                {"name","MongoDB"},
+                {"type","Database"},
+                {"count",1},
+                {"info",new BsonDocument{{"x",203},{"y",102}}}
+            };
+            //插入数据
+            collection.InsertOne(document);
+
+            var count = collection.Count(document);
+            Console.WriteLine(count);
+
+            //查询数据
+            var document1 = collection.Find(document);
+            Console.WriteLine(document1.ToString());
+
+            //更新数据
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "MongoDB");
+            var update = Builders<BsonDocument>.Update.Set("name", "Ghazi");
+
+            collection.UpdateMany(filter, update);
+
+            //删除数据
+            var filter1 = Builders<BsonDocument>.Filter.Eq("count", 101);
+
+            collection.DeleteMany(filter1);
+
+            BsonDocument document2 = new BsonDocument();
+            document2.Add("name", "MongoDB");
+            document2.Add("type", "Database");
+            document2.Add("count", "1");
+
+            collection.InsertOne(document2);
+        }
     }
 
 
@@ -61,4 +130,5 @@ namespace TestDemo
         public DateTime birthday { get; set; }
         public string passengerName { get; set; }
     }
+
 }
